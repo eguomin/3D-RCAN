28d41ee66bb0cb500f3902bfa5e8c46de72263f8
eguomin <31596377+eguomin@users.noreply.github.com>
2021-05-29 15:40:38 -0400

    add old and new apply.py

--- apply_new.py
+++ apply_new.py
@@ -1,8 +1,15 @@
-# Copyright 2020 DRVision Technologies LLC.
+# Copyright 2021 SVision Technologies LLC.
 # Creative Commons Attribution-NonCommercial 4.0 International Public License
 # (CC BY-NC 4.0) https://creativecommons.org/licenses/by-nc/4.0/
 
-from rcan.utils import apply, get_model_path, normalize, load_model
+from rcan.utils import (
+    apply,
+    convert_to_multi_gpu_model,
+    get_model_path,
+    load_model,
+    normalize,
+    rescale,
+    save_tiff)
 
 import argparse
 import itertools
@@ -15,18 +22,38 @@ def tuple_of_ints(string):
     return tuple(int(s) for s in string.split(','))
 
 
+def percentile(x):
+    x = float(x)
+    if 0.0 <= x <= 100.0:
+        return x
+    else:
+        raise argparse.ArgumentTypeError(f'{x} not in range [0.0, 100.0]')
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--model_dir', type=str, required=True)
 parser.add_argument('-i', '--input', type=str, required=True)
 parser.add_argument('-o', '--output', type=str, required=True)
+parser.add_argument(
+    '-f', '--output_tiff_format', type=str,
+    choices=['imagej', 'ome'], default='imagej')
 parser.add_argument('-g', '--ground_truth', type=str)
 parser.add_argument('-b', '--bpp', type=int, choices=[8, 16, 32], default=32)
-parser.add_argument('-B', '--block_shape', type=tuple_of_ints, default=None)
+parser.add_argument('-B', '--block_shape', type=tuple_of_ints)
+parser.add_argument('-O', '--block_overlap_shape', type=tuple_of_ints)
+parser.add_argument('--p_min', type=percentile, default=2.0)
+parser.add_argument('--p_max', type=percentile, default=99.9)
+parser.add_argument('--rescale', action='store_true')
 parser.add_argument(
     '-O', '--block_overlap_shape', type=tuple_of_ints, default=None)
 parser.add_argument(
     '-S', '--scale_value', type=float, default=None)
 args = parser.parse_args()
+
+if args.rescale and args.normalize_output_range_between_zero_and_one:
+    raise ValueError(
+        'You cannot set both `rescale` and '
+        '`normalize_output_range_between_zero_and_one` at the same time')
 
 input_path = pathlib.Path(args.input)
 output_path = pathlib.Path(args.output)
@@ -64,7 +91,8 @@ else:
 
 model_path = get_model_path(args.model_dir)
 print('Loading model from', model_path)
-model = load_model(str(model_path), input_shape=args.block_shape)
+model = convert_to_multi_gpu_model(
+    load_model(str(model_path), input_shape=args.block_shape))
 
 if args.block_overlap_shape is None:
     overlap_shape = [
@@ -80,7 +108,7 @@ else:
 
 for raw_file, gt_file in data:
     print('Loading raw image from', raw_file)
-    raw = normalize(tifffile.imread(str(raw_file)))
+    raw = normalize(tifffile.imread(str(raw_file)), args.p_min, args.p_max)
 
     print('Applying model')
     restored = apply(model, raw, overlap_shape=overlap_shape, verbose=True)
@@ -92,7 +120,10 @@ for raw_file, gt_file in data:
         print('Loading ground truth image from', gt_file)
         gt = tifffile.imread(str(gt_file))
         if raw.shape == gt.shape:
-            result.append(normalize(gt))
+            gt = normalize(gt, args.p_min, args.p_max)
+            if args.rescale:
+                restored = rescale(restored, gt)
+            result = [raw, restored, gt]
         else:
             print('Ground truth image discarded due to image shape mismatch')
 
@@ -104,8 +135,6 @@ for raw_file, gt_file in data:
     #
 
     result = np.stack(result)
-    if result.ndim == 4:
-        result = np.transpose(result, (1, 0, 2, 3))
 
     if args.bpp == 8:
         result = np.clip(sValue * result, 0, 255).astype('uint8')
